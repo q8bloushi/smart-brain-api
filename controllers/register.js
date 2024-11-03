01c124146a9c5bf928f0d467fe5891e13069b9d9
Mohammad Ahmed <71899593+q8bloushi@users.noreply.github.com>
2024-11-04 01:22:12 +0300

    Update register.js

--- controllers/register.js
+++ controllers/register.js
@@ -5,28 +5,30 @@ const handleRegister =(req, res, db, bcrypt) => {
 	}
 	const hash = bcrypt.hashSync(password);
 	db.transaction(trx => {
-		trx.insert({
-			hash: hash,
-			email: email
-		}).into('login')
-			.returning('email')
-			.then(loginEmail => {
-				trx('users')
-					.returning('*')
-					.insert({
-						email: loginEmail[0],
-						name: name,
-						joined: new Date()
-					}).then(user => {
-						res.json(user[0]);
-					})
-			})
-			.then(trx.commit)
-			.catch(trx.rollback)
-	})
-
-		.catch(err => res.status(400).json('unable to register'))
-}
+	    trx.insert({
+	        hash: hash,
+	        email: email
+	    }).into('login')
+	        .returning('email')
+	        .then(loginEmail => {
+	            return trx('users')
+	                .returning('*')
+	                .insert({
+	                    email: loginEmail[0],
+	                    name: name,
+	                    joined: new Date()
+	                });
+	        })
+	        .then(user => {
+	            trx.commit(); // Commit after successful insertion
+	            res.json(user[0]);
+	        })
+	        .catch(err => {
+	            trx.rollback(); // Rollback on error
+	            console.error(err);
+	            res.status(400).json('unable to register');
+	        });
+	});
 
 module.exports = {
     handleRegister: handleRegister
