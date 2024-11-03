8f2280c2f44268c54741fa87052a28297c7cb65b
Mohammad Ahmed <71899593+q8bloushi@users.noreply.github.com>
2024-11-04 01:30:18 +0300

    Update register.js

--- controllers/register.js
+++ controllers/register.js
@@ -1,40 +1,37 @@
-const handleRegister = (req, res, db, bcrypt) => {
-    const { email, name, password } = req.body;
-    if (!email || !name || !password) {
-        return res.status(400).json('incorrect form submission');
-    }
-    const hash = bcrypt.hashSync(password);
+const handleRegister =(req, res, db, bcrypt) => {
+	const { email, name, password } = req.body;
+	if(!email || !name || !password ){
+		return res.status(400).json('incorrect form submission');
+	}
 
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users')
-                .returning('*')
-                .insert({
-                    email: loginEmail[0],
-                    name: name,
-                    joined: new Date()
-                });
-        })
-        .then(user => {
-            trx.commit(); // Commit after successful insertion
-            res.json(user[0]);
-        })
-        .catch(err => {
-            trx.rollback(); // Rollback on error
-            console.error(err);
-            res.status(400).json('unable to register');
-        });
-    })
-    .catch(err => {
-        console.error(err); // Log the error for debugging
-        res.status(400).json('unable to register');
-    });
+    const handleRegister =(req, res, db, bcrypt) => {
+	const { email, name, password } = req.body;
+	if(!email || !name || !password ){
+		return res.status(400).json('incorrect form submission');
+	}
+	const hash = bcrypt.hashSync(password);
+	db.transaction(trx => {
+		trx.insert({
+			hash: hash,
+			email: email
+		}).into('login')
+			.returning('email')
+			.then(loginEmail => {
+				trx('users')
+					.returning('*')
+					.insert({
+						email: loginEmail[0],
+						name: name,
+						joined: new Date()
+					}).then(user => {
+						res.json(user[0]);
+					})
+			})
+			.then(trx.commit)
+			.catch(trx.rollback)
+	})
+
+		.catch(err => res.status(400).json('unable to register'))
 }
 
 module.exports = {
